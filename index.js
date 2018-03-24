119780fec02c6bdcf2d321e4ac187b6f548ecaac
corytveten <corytveten@gmail.com>
2018-03-24 14:30:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+
 function theBeatlesPlay(musicians, instruments) {
   var theBand = [];
-  for (var i = 0; i < musicians.length; i++) {
+  for (var i=0; i < musicians.length; i++) {
     theBand.push(musicians[i] + " plays " + instruments[i}];)
   }
   return theBand;
