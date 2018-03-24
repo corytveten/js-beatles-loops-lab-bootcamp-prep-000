8f875d3c50145a24b660a8ae268b5d0216df8d90
corytveten <corytveten@gmail.com>
2018-03-24 14:23:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+function theBeatlesPlay(musicians, instruments) {
+  var theBand = [];
+  for (var i = 0; i < musicians.length; i++) {
+    theBand.push(musicians[i] + " plays " + instruments[i}];)
+  }
+  return theBand;
+}
